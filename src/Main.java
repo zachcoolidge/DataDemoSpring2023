a0b3abd94f05701ded4139d6af5fb4e626f90d0b
zachcoolidge <112503849+zachcoolidge@users.noreply.github.com>
2023-04-03 13:21:33 -0400

    change

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.util.Scanner;
 import java.sql.*;
 public class Main {
-    public static void main(String[] args) {//meow meow 
+    public static void main(String[] args) {//change
         Scanner sc = new Scanner(System.in);
         char input = ' ';
         DbConnection.connect();
